4cba3a0591fdebbf7f9658625588cfec97e65605
AndyAn <andy.an@ihsmarkit.com>
2019-11-11 15:41:10 -0600

    Demean vs. Standardized

--- Demean_Standardized.py
+++ Demean_Standardized.py
@@ -8,23 +8,31 @@ Trying to see Demean vs. Standardized.
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.rand(1000, 2) +1.5
+num_point = 50
+x = np.linspace(1, 6, num_point)
+noise = np.random.randn(num_point) * 3.0
+
+# let's draw: y = ax + b + noise
+a = 8
+b = 2
+y = a * x + b + noise
 
 fig, ax = plt.subplots()
-
-ax.scatter(x[:,0], x[:,1], label='Original', alpha=0.9)
+ax.scatter(x, y, label='Original', alpha=0.9)
 
 # let's de-mean the x
 x_demean = x - np.mean(x)
-ax.scatter(x_demean[:,0], x_demean[:,1], label='De-meaned', alpha=0.7)
+y_demean = y - np.mean(y)
+ax.scatter(x_demean, y_demean, label='De-meaned', alpha=0.7)
 
 # let's see de-mean & de-std
 x_stdized = (x - np.mean(x)) / np.std(x, axis=0)
-ax.scatter(x_stdized[:,0], x_stdized[:,1], label='Standardized', alpha=0.5)
+y_stdized = (y - np.mean(y)) / np.std(y, axis=0)
+ax.scatter(x_stdized, y_stdized, label='Standardized', alpha=0.5)
 
 ax.legend()
 ax.grid(True)
 
-plt.savefig('Demean-Standardized.png')
+plt.savefig('images\Demean-Standardized.png')
 plt.show()
 
